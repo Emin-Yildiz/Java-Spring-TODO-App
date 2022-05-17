73f76fd9ff18d778b433e1a87c98364a42391581
Emin-Yildiz <eminyildiz122@gmail.com>
2022-05-17 17:51:04 +0300

    add request and response

--- src/main/java/com/todo/todolist/repo/TodoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.todo.todolist.repo;
-
-import com.todo.todolist.model.TodoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepo extends JpaRepository<TodoItem, Long> {
-}
